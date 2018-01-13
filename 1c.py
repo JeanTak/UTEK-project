8cf43cb4245ec44bb08664f5e2e1650c277adda8
wbh123456 <35285095+wbh123456@users.noreply.github.com>
2018-01-13 14:58:41 -0500

    Add files via upload

---
+++ 1c.py
@@ -0,0 +1,38 @@
+def permu_code(key, rawin, op):
+    #key: list of the permutation keys
+    #message: string of the input
+    #op = 1: enctypt; op = 0: decrypt
+    
+    '''
+    permu_code(['L', 'V', 'T', 'U', 'O', 'E', 'P', 'W', 'Q', 'F', 'N', 'J', 'M', 'K', 'G', 'B', 'R', 'Z', 'I', 'D', 'S', 'A', 'X', 'H', 'Y', 'C'],'THIS SENTENCE IS A FAIRLY TYPICAL ONE AND SHOULD BE EASY TO DECRYPT.',1)
+    >>>DWQI IOKDOKTO QI L ELQZJY DYBQTLJ GKO LKU IWGSJU VO OLIY DG UOTZYBD.
+    '''
+    
+    r = []
+    for i in rawin:
+        if (ord(i)<=90 and ord(i) >=65):
+            p = ord(i)-65
+            newi = key[p]
+            r.append(newi) 
+        else:
+            r.append(i)        
+            
+    return ''.join(r)
+
+
+#--------------------------------input and ouput part--------------------------------------
+f = open('../input/1c.in','r')
+outf = open('../output/1c.out','w')
+fstr = f.read()
+#read input file and output to output file
+commandlist = fstr.split('\n')
+for command in commandlist:
+    command = command.split(' | ')
+    if (command[0] == 'ENCRYPT'):
+        op=1
+    else:
+        op =0
+    outf.write(EDncrypt(int(command[1]),command[2],op)+'\n')
+
+f.close()
+outf.close()
